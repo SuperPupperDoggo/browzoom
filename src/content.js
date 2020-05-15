691b7d292d987f0a118649eaba5bed426ecf180d
SuperPupperDoggo <65412993+SuperPupperDoggo@users.noreply.github.com>
2020-05-15 12:23:35 -0400

    Upload source code

---
+++ src/content.js
@@ -0,0 +1,8 @@
+var link = document.getElementsByClassName('desc24 webclient');
+var download = document.getElementsByClassName('download-url');
+for (var i = 0, l = link.length; i < l; i++) {
+  link[i].style = 'display: block;';
+}
+for (var i = 0, l = download.length; i < l; i++) {
+  element.parentNode.removeChild(download[i]);
+}
